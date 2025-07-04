d67a0c9d3b225863dad5d75ce0735802f0e3488a
roloff-it <michael.roloff@studium.fernuni-hagen.de>
2025-07-04 22:00:48 +0200

    final third commit

--- main.go
+++ main.go
@@ -2,7 +2,47 @@ package main
 
 import "fmt"
 
+type Person struct {
+	name string
+	age  int
+	pet  string
+}
+
+type Bicycle struct {
+	brand  string
+	typ    string // racing, mountainbike, bmx
+	weight float32
+}
+
 func main() {
-	//
-	fmt.Println("Hello World")
+	people()
+	bike()
+}
+
+func people() {
+	fmt.Println("--------- people --------\n")
+	peter := Person{
+		name: "Peter Parke",
+		age:  23,
+		pet:  "Spider",
+	}
+
+	fmt.Println(peter.pet)
+	fmt.Println(peter.name)
+	fmt.Println(peter.age)
+	//bike()
+}
+
+func bike() {
+	fmt.Println("--------- bike --------\n")
+
+	bike := Bicycle{
+		brand:  "Canondale",
+		typ:    "BMX",
+		weight: 7.2,
+	}
+
+	fmt.Println(bike.brand)
+	fmt.Println(bike.typ)
+	fmt.Println()
 }
